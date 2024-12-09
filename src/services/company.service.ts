f4c76363506d648540cd9ae3d9361a63d44f45d0
Emil Zerun <emil.zerun@student.vinci.be>
2024-12-09 16:32:46 +0100

    oublié get add sorry

---
+++ src/services/company.service.ts
@@ -0,0 +1,26 @@
+// src/app/services/company.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Company } from 'src/types/Company';
+import { Form } from 'src/types/Form';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CompanyService {
+  private apiUrl = 'http://127.0.0.1:3000/company'; // Replace with your API endpoint
+  private formsApiUrl = 'http://127.0.0.1:3000/forms/company'
+
+  constructor(private http: HttpClient) {}
+
+  getCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(this.apiUrl);
+  }
+
+  getFormStatus(companyId: number): Observable<number> {
+    console.log(`Fetching status for company ID: ${companyId}`);
+
+    return this.http.get<number>(this.apiUrl+`/${companyId}/forms/status`);
+  }
+}
