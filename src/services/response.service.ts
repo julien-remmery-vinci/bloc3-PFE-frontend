98a395c68ae359049b9c1610febe2b0167d6d69b
Traqnar <nariman.hasanpour.idb@gmail.com>
2024-12-10 03:05:17 +0100

    Created new call in response service to save each answer in the form TO BE DONE

--- src/services/response.service.ts
+++ src/services/response.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {AnswerEsg} from "../types/AnswerEsg";
+import {AnswerPayload} from "../types/answer-payload";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResponseService {
+  private apiUrl = 'http://127.0.0.1:3000/answer/';
 
-  constructor() { }
+  constructor(private http :HttpClient ) { }
+
+  sendAnswerById(answerData:AnswerEsg): Observable<any> {
+    let answerPayload : AnswerPayload
+    //answerPayload.form_id =
+    return this.http.post<any>(`${this.apiUrl}/${answerData.answer_id}`,answerData);
+  }
 }
