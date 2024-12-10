e8f82c5c46c1a7b05732dec6a9ac5a414504ec4d
Traqnar <nariman.hasanpour.idb@gmail.com>
2024-12-10 03:10:02 +0100

    Form page, still need to implemente the logic for saving each question + submitting forms + the display of questions by category and or sub category.

--- src/pages/forms-esg-complete/forms-esg-complete.component.ts
+++ src/pages/forms-esg-complete/forms-esg-complete.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Form } from 'src/types/Form';
 import { CommonModule } from '@angular/common';
+import {QuestionWithAnswer} from "../../types/QuestionWithAnswer";
+import {ThemeService} from "../../app/theme/theme.service";
+import {FormsModule} from "@angular/forms";
+import {ResponseService} from "../../services/response.service";
 
 
 @Component({
@@ -9,41 +13,79 @@ import { CommonModule } from '@angular/common';
   templateUrl: './forms-esg-complete.component.html',
   styleUrls: ['./forms-esg-complete.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule]
 })
 
 export class FormsEsgCompleteComponent {
   public form: Form;
   public formEnd = false;
+  questions: QuestionWithAnswer[] = []; // Array of questions
+  selectedQuestion: QuestionWithAnswer | null = null; // Selected question
+  public selectedAnswerIds: Set<number> = new Set<number>(); // Set to store selected answer IDs
+  public selectedAnswerId: number | null = null; // Store the selected answer ID
 
   // TODO : Recuperer les questions de l'API
-  public questions = [
-    {category: 'Exemple de catégorie', text: 'Questions exemple n°1', answers: ['Exemple de réponse 1', 'Exemple de réponse 2', 'Exemple de réponse 3']},
-    {category: 'Exemple de catégorie', text: 'Questions exemple n°2', answers: ['Exemple de réponse 1', 'Exemple de réponse 2', 'Exemple de réponse 3']},
-    {category: 'Exemple de catégorie', text: 'Questions exemple n°3', answers: ['Exemple de réponse 1', 'Exemple de réponse 2', 'Exemple de réponse 3']},
-  ];
+  public selectedCategory :any = null ;
+      constructor(private router: Router, protected themeService:ThemeService,private responseService:ResponseService) {
+      const navigation = this.router.getCurrentNavigation();
+      this.form = navigation?.extras?.state?.['form'];
+      this.questions = this.form.questions;
+      this.selectedQuestion = this.questions[0];
+      this.selectedAnswerIds.clear();
+      this.updateSelectedAnswerId();
 
-  public selectedQuestion = this.questions[0];
-  
-  constructor(private router: Router) {
-    const navigation = this.router.getCurrentNavigation();
-    this.form = navigation?.extras?.state?.['form'];
+      if(this.selectedQuestion?.user_answers){
+        for (const answer of this.selectedQuestion.user_answers) {
+          this.selectedAnswerIds.add(answer.answer_id);
+        }
+      }
+      console.log(this.form)
   }
-
+  updateSelectedAnswerId() {
+    if (this.selectedQuestion?.user_answers && this.selectedQuestion.user_answers.length > 0) {
+      this.selectedAnswerId = this.selectedQuestion.user_answers[0].answer_id;
+    } else {
+      this.selectedAnswerId = null;
+    }
+  }
+  private saveQuestion(question:QuestionWithAnswer) {
+    //this.responseService.sendAnswerById(question)
+  }
   selectQuestion(question: any) {
-    this.selectedQuestion = question;
+      //  this.saveQuestion();
+        this.selectedQuestion = question;
+        this.selectedAnswerIds.clear();
+        this.updateSelectedAnswerId();
+        console.log(this.selectedQuestion)
+
+        if(this.selectedQuestion?.user_answers){
+          for (const answer of this.selectedQuestion.user_answers) {
+            this.selectedAnswerIds.add(answer.answer_id);
+          }
+        }
+        console.log(this.selectedAnswerIds)
+        if(this.form && this.questions && this.selectedQuestion) {
+          const index = this.questions.indexOf(this.selectedQuestion);
+          if (index + 1 === this.questions.length) {
+            this.formEnd = true;
+          } else {
+            this.formEnd = false;
+          }
+    }
+
   }
 
   // TODO : Envoyer la réponse à l'API
   nextQuestion(answer: any) {
-    const index = this.questions.indexOf(this.selectedQuestion);
-    this.selectedQuestion = this.questions[index + 1];
-    if (index + 1 === this.questions.length - 1) {
-      this.formEnd = true;
+    if(this.form && this.questions && this.selectedQuestion){
+      const index = this.questions.indexOf(this.selectedQuestion);
+      this.selectQuestion(this.questions[index + 1]);
     }
   }
 
   submitForm(form: Form) {
     alert('A implementer');
   }
+
+
 }
