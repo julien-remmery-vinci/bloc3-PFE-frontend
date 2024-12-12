7be3480033a39d23ebbc998f441abb92e2cd620b
Semih <semih.turkoglu@student.vinci.be>
2024-12-12 05:24:28 +0100

    feat: fixing error due to dynamic data

--- src/pages/statistics/statistics.component.ts
+++ src/pages/statistics/statistics.component.ts
@@ -1,84 +1,123 @@
-import { Component, OnInit } from "@angular/core";
-import * as Highcharts from "highcharts";
-import { StatsService } from "../../services/stats.service";
-import { Stats } from "../../types/Stats";
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { StatsService } from '../../services/stats.service';
+import { Stats } from '../../types/stats';
 
 @Component({
-  selector: "app-statistics",
-  templateUrl: "./statistics.component.html",
-  styleUrls: ["./statistics.component.css"],
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
-  chartOptions: Highcharts.Options = {};
-  pieChartOptions: Highcharts.Options = {};
+  chartOptions: Highcharts.Options;
+  pieChartOptions: Highcharts.Options;
+  updateFlag = false;
   stats: Stats | null = null;
 
-  constructor(private statsService: StatsService) {}
+  constructor(private statsService: StatsService) {
+    this.chartOptions = {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'Pourcentage des entreprises par type de template'
+      },
+      xAxis: {
+        categories: ['OWNED FACILITY', 'WORKERS', 'PRODUITS', 'FACILITY']
+      },
+      yAxis: {
+        title: {
+          text: 'Pourcentage'
+        }
+      },
+      series: [{
+        type: 'column',
+        name: 'Pourcentage',
+        data: [0, 0, 0, 0]
+      }]
+    };
+
+    this.pieChartOptions = {
+      chart: {
+        type: 'pie'
+      },
+      title: {
+        text: 'Répartition des rôles des utilisateurs'
+      },
+      series: [{
+        type: 'pie',
+        name: 'Utilisateurs',
+        data: [
+          { name: 'Admin', y: 0 },
+          { name: 'User', y: 0 }
+        ]
+      }]
+    };
+  }
 
   ngOnInit(): void {
+    this.loadStats();
+  }
+
+  loadStats(): void {
     this.statsService.getStats().subscribe((stats: Stats) => {
       this.stats = stats;
       this.updateChartOptions();
-      console.log(stats);
+      this.updatePieChartOptions();
+      this.updateFlag = true;
     });
   }
 
   updateChartOptions(): void {
     if (!this.stats) return;
 
-    const totalTemplates =
-      this.stats.total_templates_owned_facility +
-      this.stats.total_templates_workers +
-      this.stats.total_templates_products +
-      this.stats.total_templates_facility;
-
     this.chartOptions = {
       chart: {
-        type: "column",
+        type: 'column'
       },
       title: {
-        text: "Pourcentage des entreprises par type de template",
+        text: 'Pourcentage des entreprises par type de template'
       },
       xAxis: {
-        categories: ["OWNED FACILITY", "WORKERS", "PRODUITS", "FACILITY"],
+        categories: ['OWNED FACILITY', 'WORKERS', 'PRODUITS', 'FACILITY']
       },
       yAxis: {
         title: {
-          text: "Pourcentage",
-        },
+          text: 'Pourcentage'
+        }
       },
-      series: [
-        {
-          type: "column",
-          name: "Pourcentage",
-          data: [
-            (this.stats.total_templates_owned_facility / totalTemplates) * 100,
-            (this.stats.total_templates_workers / totalTemplates) * 100,
-            (this.stats.total_templates_products / totalTemplates) * 100,
-            (this.stats.total_templates_facility / totalTemplates) * 100,
-          ],
-        },
-      ],
+      series: [{
+        type: 'column',
+        name: 'Pourcentage',
+        data: [
+          this.stats.total_templates_owned_facility,
+          this.stats.total_templates_workers,
+          this.stats.total_templates_products,
+          this.stats.total_templates_facility
+        ]
+      }]
     };
+  }
+
+  updatePieChartOptions(): void {
+    if (!this.stats) return;
 
     this.pieChartOptions = {
       chart: {
-        type: "pie",
+        type: 'pie'
       },
       title: {
-        text: "Répartition des rôles des utilisateurs",
+        text: 'Répartition des rôles des utilisateurs'
       },
-      series: [
-        {
-          type: "pie",
-          name: "Utilisateurs",
-          data: [
-            { name: "Admin", y: 10 },
-            { name: "User", y: 90 },
-          ],
-        },
-      ],
+      series: [{
+        type: 'pie',
+        name: 'Utilisateurs',
+        data: [
+          { name: 'Admin', y: this.stats.total_accepted_onboarding },
+          { name: 'User', y: this.stats.total_rejected_onboarding }
+        ]
+      }]
     };
   }
 }
--- src/services/stats.service.ts
+++ src/services/stats.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Stats } from '../types/Stats';
+import { Stats } from '../types/stats';
 
 @Injectable({
   providedIn: 'root'
